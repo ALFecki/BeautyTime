c0844251ee92304487033f1c5c48b916da9e66f9
Skvortsov A.V <alexskvr03@gmail.com>
2023-12-01 20:54:00 +0300

    product added

---
+++ src/models/product_entity.py
@@ -0,0 +1,12 @@
+from sqlalchemy import REAL, DateTime, Integer, String
+from sqlalchemy.orm import Mapped, mapped_column
+from src.base.base_model import BaseEntity
+
+class Product(BaseEntity):
+    name: Mapped[str] = mapped_column(String, nullable=False)
+    alias: Mapped[str] = mapped_column(String, nullable=False)
+    quantity: Mapped[int] = mapped_column(Integer, nullable=False)
+    supply_date: Mapped[DateTime] = mapped_column(DateTime(timezone=True), nullable=False)
+    client_cost: Mapped[REAL] = mapped_column(REAL, nullable=False)
+    supply_cost: Mapped[REAL] = mapped_column(REAL, nullable=False)
+    cost_diff: Mapped[REAL] = mapped_column(REAL, nullable=False)
