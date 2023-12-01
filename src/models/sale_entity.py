b495c99e1ec84948455f9e60ce7c92e7538daa20
Skvortsov A.V <alexskvr03@gmail.com>
2023-12-01 20:56:58 +0300

    sale model

---
+++ src/models/sale_entity.py
@@ -0,0 +1,9 @@
+from sqlalchemy import DateTime, ForeignKey, Integer
+from sqlalchemy.orm import Mapped, mapped_column
+from src.base.base_model import BaseEntity
+
+class Sale(BaseEntity):
+    client_id: Mapped[int] = mapped_column(ForeignKey("client.id"))
+    product_id: Mapped[int] = mapped_column(ForeignKey("product.id"))
+    quantity: Mapped[int] = mapped_column(Integer, nullable=False)
+    sale_date: Mapped[DateTime] = mapped_column(DateTime(timezone=True), nullable=False)
