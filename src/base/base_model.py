0485c74ffb4163a8c66285ad95e3c8dba64dc333
Skvortsov A.V <alexskvr03@gmail.com>
2023-12-03 21:43:33 +0300

    base model fixes

--- src/base/base_model.py
+++ src/base/base_model.py
@@ -11,6 +11,6 @@ class BaseEntity(Base):
 
     __abstract__ = True
     id: Mapped[int] = mapped_column(primary_key=True)
-    create_date: Mapped[DateTime] = mapped_column(
-        DateTime(timezone=True), server_default=func.now()
-    )
+    # create_date: Mapped[DateTime] = mapped_column(
+    #     DateTime(timezone=True), server_default=func.now()
+    # )
