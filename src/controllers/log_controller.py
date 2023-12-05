cf670fb754d301b11934208c073f566dabe0a822
Skvortsov A.V <alexskvr03@gmail.com>
2023-12-05 16:01:55 +0300

    log c + r + s

---
+++ src/controllers/log_controller.py
@@ -0,0 +1,45 @@
+from fastapi import APIRouter, Depends, Path
+from src.schemas.log.log_schema_update import LogSchemaUpdate
+
+from src.services.log_service import LogService
+from src.schemas.log.log_schema_create import LogSchemaCreate
+
+
+router = APIRouter(prefix="/api/log", tags=["log"])
+
+
+@router.get("/")
+async def get_all_logs(service=Depends(LogService)):
+    return await service.get_all()
+
+
+@router.get("/{id}")
+async def get_log_by_id(
+    id: int = Path(example=1, description="ID искомого лога"),
+    service=Depends(LogService),
+):
+    return await service.get_by_id(id)
+
+
+@router.post("/")
+async def create_log(
+    create_schema: LogSchemaCreate, service=Depends(LogService)
+):
+    return await service.create(create_schema)
+
+
+@router.patch("/{id}")
+async def update_log(
+    update_schema: LogSchemaUpdate,
+    id: int = Path(example=1, description="ID обновляемого лога"),
+    service=Depends(LogService),
+):
+    return await service.update(id, update_schema)
+
+
+@router.delete("/{id}")
+async def delete_log(
+    id: int = Path(example=1, description="ID удаляемого лога"),
+    service=Depends(LogService),
+):
+    return await service.delete(id)
---
+++ src/repositories/log_repository.py
@@ -0,0 +1,61 @@
+from sqlalchemy import Row, text
+from src.base.base_repository import BaseRepo
+from src.base.base_service import AsyncSession
+from src.models.log_entity import Log
+from src.repositories.client_repository import ClientRepository
+from src.schemas.log.log_schema import LogSchema
+from src.schemas.log.log_schema_create import LogSchemaCreate
+from src.schemas.log.log_schema_update import LogSchemaUpdate
+from src.utils.not_found_exception import NotFoundException
+
+
+class LogRepository(BaseRepo):
+    @property
+    def model(self) -> type[Log]:
+        return Log
+
+    @property
+    def schema(self) -> type[LogSchema]:
+        return LogSchema
+
+    @property
+    def create_schema(self) -> type[LogSchemaCreate]:
+        return LogSchemaCreate
+
+    @property
+    def update_schema(self) -> type[LogSchemaUpdate]:
+        return LogSchemaUpdate
+    
+
+    async def create_response(self, row: Row, session: AsyncSession):
+        fields_dict = row._asdict()
+
+        repo = ClientRepository()
+        fields_dict["client"] = await repo.get_by_id(session, fields_dict["client_id"])
+
+        return self.schema(**fields_dict)
+
+    async def get_all(self, session: AsyncSession):
+        statement = text(f"""SELECT * FROM {self.model.__tablename__}""")
+        res = (await session.execute(statement)).fetchall()
+        if res is None:
+            raise NotFoundException(
+                404,
+                "Объект не найден",
+                self.model.__name__ + " with current ID: " + str(id) + " was not found",
+            )
+        return [await self.create_response(obj, session) for obj in res]
+
+    async def get_by_id(self, session: AsyncSession, id: int) -> LogSchema:
+        statement = text(
+            f"""SELECT * FROM {self.model.__tablename__}
+                WHERE public.{self.model.__tablename__}.id = {id};"""
+        )
+        res = (await session.execute(statement)).fetchone()
+        if res is None:
+            raise NotFoundException(
+                404,
+                "Объект не найден",
+                self.model.__name__ + " with current ID: " + str(id) + " was not found",
+            )
+        return await self.create_response(res, session)
